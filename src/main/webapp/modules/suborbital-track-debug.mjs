6baf754cd5c1a1e1248bdb6c29292a581df977b4
Bretton Wade <bretton.wade@gmail.com>
2022-11-30 14:18:22 -0500

    main - include handling the update vis callback

--- src/main/webapp/modules/suborbital-track-debug.mjs
+++ src/main/webapp/modules/suborbital-track-debug.mjs
@@ -421,6 +421,7 @@ $.addTle = function (idsToShow) {
     });
     $.updateVis = function (idsToShow, timeToShow) {
         LogLevel.info ("Update Vis called with " + idsToShow.length + " elements, at " + timeToShow.toString());
+        this.addTle (idsToShow, timeToShow);
     };
     return $;
 };
