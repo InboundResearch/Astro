ac6206d1c7dc4deb9fbe364ca37d0d8dcbdba2cb
Bretton Wade <bretton.wade@gmail.com>
2022-11-30 14:04:32 -0500

    main - add in "smarts" for Jeff

--- src/main/webapp/modules/suborbital-track-index.mjs
+++ src/main/webapp/modules/suborbital-track-index.mjs
@@ -1,13 +1,25 @@
 import {SuborbitalTrack} from "./suborbital-track.mjs";
 
-let suborbitalTrack;
+let updateVisData = null;
+let suborbitalTrack = null;
+
 window.addEventListener ("load", event => {
     SuborbitalTrack ("render-canvas-div", suborbitalTrackIn => {
         suborbitalTrack = suborbitalTrackIn;
     });
+
+    if (updateVisData) {
+        suborbitalTrack.updateVis(updateVisData.idsToShow,
+            ("timeToShow") in updateVisData ? updateVisData.timeToShow : Date.now());
+        updateVisData = null;
+    }
 });
 
 window.addEventListener("message", event => {
-    suborbitalTrack.updateVis(event.data.idsToShow,
-        ("timeToShow") in event.data ? event.data.timeToShow : Date.now());
+    if (suborbitalTrack) {
+        updateVisData = event.data;
+    } else {
+        suborbitalTrack.updateVis(event.data.idsToShow,
+            ("timeToShow") in event.data ? event.data.timeToShow : Date.now());
+    }
 });
